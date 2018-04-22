842d153e63370b1a786aa788b09c3bb8c0d60d82
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-04-22 09:21:40 +0900

    solve ARC096C - Half and Half in contest

---
+++ solutions/ARC096C-HalfAndHalf.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+
+/*
+ 
+ 4/22/2018 ARC096
+ 
+ 21:00-21:18 AC
+ 
+ Editorials:
+  - https://img.atcoder.jp/arc096/editorial.pdf
+  - https://www.youtube.com/watch?v=qOG8pkCP7oI
+  - https://togetter.com/li/1220212
+ 
+ */
+LL solve(int A, int B, int C, int X, int Y) {
+  LL res=1e10;
+  REPE(c,2*max(X,Y)) {
+    LL a=max(0,X-c/2);
+    LL b=max(0,Y-c/2);
+    SMIN(res,a*A+b*B+c*C);
+  }
+  return res;
+}
+
+int main() {
+  int A,B,C,X,Y;
+  cin >> A >> B >> C >> X >> Y;
+  cout << solve(A,B,C,X,Y) << endl;
+  return 0;
+}
