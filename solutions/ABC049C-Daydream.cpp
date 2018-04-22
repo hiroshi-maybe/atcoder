50cca396c9ff6b0202ca63b49f629af41a61f956
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-04-22 15:30:29 +0900

    solve ABC049C - Daydream

---
+++ solutions/ABC049C-Daydream.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+
+/*
+ 
+ 4/22/2018
+ 
+ 15:07-15:15 ACC
+ 
+ */
+
+// $ g++ -std=c++11 -Wall -O2 -D_GLIBCXX_DEBUG x.cpp && ./a.out
+string yes="YES",no="NO";
+string solve(string S) {
+  int N=SZ(S);
+  reverse(S.begin(),S.end());
+  vector<string> D={ "dream","dreamer","erase","eraser" };
+  FORR(s,D) reverse(s.begin(),s.end());
+  int i=0;
+  while(i<N) {
+    bool ok=false;
+    FORR(s,D) if(N-i>=SZ(s)&&S.substr(i,SZ(s))==s) {
+      i+=SZ(s);
+      ok=true;
+      break;
+    }
+    if(!ok) return no;
+  }
+  return i==N?yes:no;
+}
+
+int main() {
+  string S;
+  cin>>S;
+  cout<<solve(S)<<endl;
+  return 0;
+}
