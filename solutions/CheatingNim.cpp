136dbbb67c006c17e2b3454de97e91ea9537e6db
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-08-10 13:34:56 -0700

    solve CODE FESTIVAL 2016 Grand Final C - Cheating Nim

---
+++ solutions/CheatingNim.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+
+/*
+ 
+ 8/9/2018
+ 
+ 22:15-22:30 analysis
+ 22:30-22:40 implement and got AC
+ 
+ Editorials:
+  - https://cf16-final.contest.atcoder.jp//data/other/cf16-exhibition-final/editorial.pdf
+  - https://kimiyuki.net/writeup/algo/atcoder/codefestival-2016-grand-final-c/
+  - http://kenkoooo.hatenablog.com/entry/2018/04/18/043729
+ 
+ */
+
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG x.cpp && ./a.out
+const int MAX_N=1e5+1;
+int N;
+LL A[MAX_N];
+int P[32];
+int solve() {
+  MINUS(P);
+  REP(i,N) {
+    REP(j,32) if((A[i]>>j)&1) {
+      P[j]=i;
+      break;
+    }
+  }
+//  dumpAR(P);
+  int res=0;
+  for(int b=31; b>=0; --b) {
+    int cnt=0;
+    REP(i,N) cnt+=(A[i]>>b)&1;
+    if((cnt&1)==1) {
+//      dump3(b,cnt,P[b]);
+      if(P[b]==-1) return -1;
+      A[P[b]]-=1,res+=1;
+    }
+  }
+  return res;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  cin>>N;
+  REP(i,N) cin>>A[i];
+  cout<<solve()<<endl;
+  return 0;
+}
