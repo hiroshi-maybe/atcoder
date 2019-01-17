1a7413a0a2537e79c00cd0730a176a85309abfc6
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-01-16 20:25:51 -0800

    solve ABC054 B - Template Matching

---
+++ solutions/TemplateMatching.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+#include <iomanip>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<vector<int>> VVI;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpf(args...) if(TRACE) { fprintf(stderr, ##args); putchar('\n'); }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+template<class Iter> void dumpc(Iter begin, Iter end) { if (TRACE) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; } }
+
+// $ cp-batch TemplateMatching | diff TemplateMatching.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address TemplateMatching.cpp && ./a.out
+
+/*
+ 
+ 1/16/2019
+ 
+ 18:37-18:43 AC
+ 
+ https://img.atcoder.jp/abc054/editorial.pdf
+ 
+ */
+
+const int MAX_N=51;
+string A[MAX_N],B[MAX_N];
+int N,M;
+
+const string yes="Yes",no="No";
+void solve() {
+  bool res=false;
+  REP(i,N-M+1)REP(j,N-M+1) {
+    bool ok=true;
+    REP(di,M)REP(dj,M) {
+      if(A[i+di][j+dj]!=B[di][dj]) {
+        ok=false;
+        goto out1;
+      }
+    }
+  out1:
+    if(ok) {
+      res=true;
+      goto out2;
+    }
+  }
+out2:
+  cout<<(res?yes:no)<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N>>M;
+  REP(i,N) cin>>A[i];
+  REP(i,M) cin>>B[i];
+  solve();
+  
+  return 0;
+}
