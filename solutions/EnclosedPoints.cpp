9cd36cc0696375699367d9565c935e37adc03c77
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-08-08 17:19:25 +0900

    solve ABC136 F - Enclosed Points

---
+++ solutions/EnclosedPoints.cpp
@@ -0,0 +1,192 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+//const int MOD=1000000007;
+const int MOD=998244353;
+struct ModInt {
+  unsigned int val;
+  ModInt(): val(0) {}
+  ModInt(int v) { norm(v%MOD); }
+  ModInt(long long v) { norm(v%MOD); }
+  ModInt& norm(long long v) {
+    v=v<0?v%MOD+MOD:v; // negative
+    v=v>=MOD?v-MOD:v; // mod
+    val=(unsigned int)v;
+    return *this;
+  }
+  explicit operator bool() const { return val!=0; }
+  ModInt operator-() const { return ModInt(0)-*this; }
+  ModInt &operator+=(ModInt that) { return norm((long long)val+that.val); }
+  ModInt &operator-=(ModInt that) { return norm((long long)val-that.val); }
+  ModInt &operator*=(ModInt that) { val=(unsigned long long)val*that.val%MOD; return *this; }
+  ModInt &operator/=(ModInt that) { return *this*=that.inv(); }
+  ModInt operator+(ModInt that) const { return ModInt(*this)+=that; }
+  ModInt operator-(ModInt that) const { return ModInt(*this)-=that; }
+  ModInt operator*(ModInt that) const { return ModInt(*this)*=that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this)/=that; }
+  ModInt pow(long long n) const {
+    ModInt x=*this, res=1;
+    while(n>0) {
+      if(n&1) res*=x;
+      x*=x,n>>=1;
+    }
+    return res;
+  }
+  ModInt inv() const { return (*this).pow(MOD-2); }
+  bool operator==(ModInt that) const { return val==that.val; }
+  bool operator!=(ModInt that) const { return val!=that.val; }
+  friend ostream& operator<<(ostream& os, const ModInt& that) { return os<<that.val; }
+};
+template <typename T> struct BIT {
+public:
+  int N;
+  vector<T> A;
+  BIT() {}
+  BIT(int N) { init(N); }
+  void init(int N) {
+    this->N=N;
+    A=vector<T>(N+1,0);
+  }
+  // query ∑ { sum[i] : i=l..<r }
+  T query(int l, int r) {
+    assert(l<=r&&0<=l&&r<=N);
+    return query(r)-query(l);
+  }
+  // min index s.t. dat[0..i]>x
+  int upperbound(T x) {
+    int good=N+1,bad=0;
+    while(abs(good-bad)>1) {
+      int m=(good+bad)/2;
+      (query(m)>x?good:bad)=m;
+    }
+    return good-1;
+  }
+  // sum[i]+=x
+  void add(int i, T x) {
+    assert(0<=i&&i<N);
+    ++i;
+    while(i<=N) A[i]+=x,i+=lsone(i);
+  }
+  // sum[i]=x
+  void update(int i, T v) {
+    T pre=query(i+1)-query(i);
+    add(i,v-pre);
+  }
+private:
+  int lsone(int i) { return i&-i; }
+  // query in [0,r) : ∑ { sum[i] : i=0..r-1 }
+  T query(int r) {
+    assert(0<=r&&r<=N);
+    T res=0;
+    while(r>0) res+=A[r],r-=lsone(r);
+    return res;
+  }
+};
+
+// $ cp-batch EnclosedPoints | diff EnclosedPoints.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address EnclosedPoints.cpp && ./a.out
+
+/*
+ 
+ 8/6/2019
+ 
+ 15:40-17:10, 20:05-20:37 AC
+ 
+ It took much time to come up with counting by principle of inclusion exclusion.
+ 
+ https://img.atcoder.jp/abc136/editorial.pdf
+ 
+ */
+
+const int MAX_N=1e6+1;
+int X[MAX_N],Y[MAX_N];
+int N;
+
+ModInt f(int x) {
+  return ModInt(2).pow(x)-1;
+}
+void solve() {
+  vector<II> ps;
+  REP(i,N) ps.emplace_back(X[i],Y[i]);
+  sort(ALL(ps));
+  sort(Y,Y+N);
+  map<int,int> yinv; REP(i,N) yinv[Y[i]]=i;
+  ModInt res;
+  BIT<int> bitl(N),bitr(N);
+  REP(i,N) bitr.add(yinv[Y[i]],1);
+  REP(i,N) {
+    int x=ps[i].first,y=ps[i].second;
+    bitr.add(yinv[y],-1);
+    int a=bitl.query(0, yinv[y]),b=bitl.query(yinv[y]+1,N);
+    int c=bitr.query(0, yinv[y]),d=bitr.query(yinv[y]+1,N);
+    /*
+    ModInt a1=ModInt(1);
+    FORR(x,VI({a,b,c,d})) a1+=f(x);
+    ModInt a2=f(a)*f(b)+f(b)*f(d)+f(d)*f(c)+f(c)*f(a);
+    ModInt a3=f(a)*f(d)*ModInt(2).pow(b+c+1)+f(b)*f(c)*ModInt(2).pow(a+d+1);
+    dump(x,y,a,b,c,d);
+    dump(a1,a2,a3);
+    res+=a1+a2+a3;
+     */
+    ModInt u=f(a+b+c+d+1);
+    ModInt a1=f(a)+f(b)+f(c)+f(d);
+    ModInt a2=f(a)*f(b)+f(b)*f(d)+f(c)*f(d)+f(c)*f(a);
+    res+=u-a1-a2;
+//    dump(u,a1,a2);
+    bitl.add(yinv[y],1);
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N;
+  REP(i,N) cin>>X[i]>>Y[i];
+  solve();
+  
+  return 0;
+}
