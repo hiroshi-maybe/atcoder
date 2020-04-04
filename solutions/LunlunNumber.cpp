25eb639de212d42a5ee6761fe347d6dbde34e190
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-04-04 11:30:43 -0700

    solve ABC161 D - Lunlun Number

---
+++ solutions/LunlunNumber.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch LunlunNumber | diff LunlunNumber.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address LunlunNumber.cpp && ./a.out
+
+/*
+ 
+ 4/4/2020
+ 
+ 5:22-6:04 AC
+ 
+ */
+
+int K;
+
+void solve() {
+  priority_queue<LL> Q;
+  FOR(i,1,10) Q.emplace(-i);
+  vector<LL> res;
+  while(SZ(res)<K) {
+    LL n=Q.top(); Q.pop();
+    n=-n;
+    res.push_back(n);
+    int dd=n%10LL;
+    LL base=10LL*n;
+    
+    auto add=[&](int d) {
+      if(d>=0&&d<=9) {
+        LL a=base+d;
+        Q.emplace(-a);
+      }
+    };
+    add(dd-1),add(dd),add(dd+1);
+  }
+//  dumpc(res);
+//  vector<LL> ans=res;
+//  if(SZ(res)>=100) dumpC(res.end()-100,res.end());
+//  sort(ALL(ans));
+//  assert(ans==res);
+  cout<<res[K-1]<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>K;
+  solve();
+  
+  return 0;
+}
