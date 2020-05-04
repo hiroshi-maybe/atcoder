b79151b26c8b3a4f9b32c22cc08cab3603de4155
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-05-03 21:04:57 -0700

    solve ARC038 B - マス目と駒

---
+++ solutions/GridAndKoma.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch GridAndKoma | diff GridAndKoma.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address GridAndKoma.cpp && ./a.out
+
+/*
+ 
+ 5/3/2020
+ 
+ 20:57-21:02 AC
+ 
+ https://www.slideshare.net/chokudai/arc038
+ 
+ */
+
+const int MAX_N=1e2+1;
+string A[MAX_N];
+int N,M;
+
+int memo[MAX_N][MAX_N];
+
+int f(int i, int j) {
+  int &res=memo[i][j];
+  if(res>=0) return res;
+  res=false;
+  if(j+1<M&&A[i][j+1]=='.') res|=!f(i,j+1);
+  if(i+1<N&&j+1<M&&A[i+1][j+1]=='.') res|=!f(i+1,j+1);
+  if(i+1<N&&A[i+1][j]=='.') res|=!f(i+1,j);
+  return res;
+}
+
+void solve() {
+  MINUS(memo);
+  cout<<(f(0,0)?"First":"Second")<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N>>M;
+  REP(i,N) cin>>A[i];
+  solve();
+  
+  return 0;
+}
