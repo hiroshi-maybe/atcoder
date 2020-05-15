b70efc928f158364e3ba8931860bd858f1284089
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-05-14 21:47:25 -0700

    solve CF2016 QC C - 二人のアルピニスト / Two Alpinists again

--- solutions/TwoAlpinists.cpp
+++ solutions/TwoAlpinists.cpp
@@ -1,34 +1,17 @@
-#include <iostream>
-#include <iomanip>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <set>
-#include <map>
-#include <iostream>
-#include <utility>
-#include <cctype>
-#include <queue>
-#include <stack>
-#include <cstdio>
-#include <cstdlib>
-#include <cmath>
-#include <unordered_set>
-#include <unordered_map>
-#include <limits.h>
-#include <cstring>
-#include <tuple>
-#include <cassert>
-#include <numeric>
+#include <bits/stdc++.h>
 using namespace std;
 // type alias
 typedef long long LL;
-typedef vector < int > VI;
-typedef unordered_map < int, int > MAPII;
-typedef unordered_set < int > SETI;
-typedef pair< int , int > II;
-typedef tuple< int, int, int > III;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
 // repetition
 #define FORE(i,a,b) for(int i=(a);i<=(b);++i)
 #define REPE(i,n)  for(int i=0;i<=(n);++i)
@@ -36,19 +19,66 @@ typedef tuple< int, int, int > III;
 #define REP(i,n)  for(int i=0;i<(n);++i)
 #define FORR(x,arr) for(auto& x:arr)
 #define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
 // DP
 #define MINUS(dp) memset(dp, -1, sizeof(dp))
 #define ZERO(dp) memset(dp, 0, sizeof(dp))
-// minmax
-#define SMAX(a,b) a = max(a,b)
-#define SMIN(a,b) a = min(a,b)
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
 // debug cerr
-#define TRACE true
-#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
-#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
-#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
-#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
-#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+const int MOD=1000000007;
+//const int MOD=998244353;
+struct ModInt {
+  unsigned int val;
+  ModInt(): val(0) {}
+  ModInt(int v) { norm(v%MOD); }
+  ModInt(long long v) { norm(v%MOD); }
+  ModInt& norm(long long v) {
+    v=v<0?v%MOD+MOD:v; // negative
+    v=v>=MOD?v-MOD:v; // mod
+    val=(unsigned int)v;
+    return *this;
+  }
+  explicit operator bool() const { return val!=0; }
+  ModInt operator-() const { return ModInt(0)-*this; }
+  ModInt &operator+=(ModInt that) { return norm((long long)val+that.val); }
+  ModInt &operator-=(ModInt that) { return norm((long long)val-that.val); }
+  ModInt &operator*=(ModInt that) { val=(unsigned long long)val*that.val%MOD; return *this; }
+  ModInt &operator/=(ModInt that) { return *this*=that.inv(); }
+  ModInt operator+(ModInt that) const { return ModInt(*this)+=that; }
+  ModInt operator-(ModInt that) const { return ModInt(*this)-=that; }
+  ModInt operator*(ModInt that) const { return ModInt(*this)*=that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this)/=that; }
+  ModInt pow(long long n) const {
+    ModInt x=*this, res=1;
+    while(n>0) {
+      if(n&1) res*=x;
+      x*=x,n>>=1;
+    }
+    return res;
+  }
+  ModInt inv() const { return (*this).pow(MOD-2); }
+  bool operator==(ModInt that) const { return val==that.val; }
+  bool operator!=(ModInt that) const { return val!=that.val; }
+  friend ostream& operator<<(ostream& os, const ModInt& that) { return os<<that.val; }
+};
+// $ cp-batch TwoAlpinists | diff TwoAlpinists.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address TwoAlpinists.cpp && ./a.out
 
 /*
  
@@ -62,30 +92,32 @@ typedef tuple< int, int, int > III;
   - http://hamko.hatenadiary.jp/entry/2017/07/28/110610
   - https://ykmaku.hatenablog.com/entry/2018/07/15/223228
   - http://competitive-kivantium.hateblo.jp/entry/2016/10/24/135521
+
+  5/14/2020
+
+  21:25-21:41 solve again
  
  */
 
-// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG x.cpp && ./a.out
-const int MAX_N=1e5+1;
+const int MAX_N=1e6+1;
+LL A[MAX_N],T[MAX_N],L[2][MAX_N],R[2][MAX_N];
 int N;
-LL A[MAX_N],T[MAX_N];
-const LL MOD=1e9+7;
-LL L[MAX_N],R[MAX_N];
+
 void solve() {
   REP(i,N) {
-    if(i==0||A[i-1]<A[i]) L[i]=R[i]=A[i];
-    else L[i]=1,R[i]=A[i];
+    if(i==0||T[i-1]!=T[i]) L[0][i]=R[0][i]=T[i];
+    else L[0][i]=1,R[0][i]=T[i];
   }
-  LL res=1;
   for(int i=N-1; i>=0; --i) {
-    LL l,r;
-    if(i==N-1||T[i]>T[i+1]) l=r=T[i];
-    else l=1,r=T[i];
-    LL d=min(r,R[i])-max(l,L[i])+1;
-    d=max(d,0LL);
-    if(d<0) res=0;
-//    dump2(i,d);
-    res*=d,res%=MOD;
+    if(i==N-1||A[i]!=A[i+1]) L[1][i]=R[1][i]=A[i];
+    else L[1][i]=1,R[1][i]=A[i];
+  }
+
+  ModInt res=1;
+  REP(i,N) {
+    LL l=max(L[0][i],L[1][i]),r=min(R[0][i],R[1][i]);
+    //dump(i,L[0][i],R[0][i],L[1][i],R[1][i],l,r,r-l);
+    res*=max(0LL,r-l+1);
   }
   cout<<res<<endl;
 }
@@ -93,10 +125,12 @@ void solve() {
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
+  cout<<setprecision(12)<<fixed;
   
   cin>>N;
-  REP(i,N) cin>>A[i];
   REP(i,N) cin>>T[i];
+  REP(i,N) cin>>A[i];
   solve();
+  
   return 0;
 }
