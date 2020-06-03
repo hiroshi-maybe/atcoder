80a995f8fddd48f24b679e5cf0e1fcfe5115ff61
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-06-03 09:56:11 -0700

    solve ABC123 D - Cake 123 again

--- solutions/Cake123.cpp
+++ solutions/Cake123.cpp
@@ -46,33 +46,33 @@ template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, con
 // $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address Cake123.cpp && ./a.out
 
 /*
- 
+
  4/7/2019
- 
+
  20:19-21:05 give up
- 
+
  4/8/2019
- 
+
  14:45-16:40 read editorials and got AC
- 
+
  There are three major solutions
   1. Make `K` largest from A and B as A'. make K largest from A' and C
   2. If f(A[i],B[j],C[k]) is taken, keep adding next candidate to priority queue
    - f(A[i+1],B[j],C[k]), f(A[i],B[j+1],C[k]) and f(A[i],B[j].C[k+1]) are next candidate
   3. Brute-force + edge pruning
- 
+
  # 1. Semi brute-force.
- 
+
  We can get A'={a+b:a∈A,b∈B} and sort in descendent order where |A'| = |A|*|B|.
  A'[0..K-1] can be a candidate of K largest. However A[K] cannot.
  The best partner for A[K] is C[0]. However A[K]+C[0]<A[i]+C[0], i=0..K-1.
  Thus Choosing K largest from A' is optimal.
- 
+
  #2. Priority queue
- 
+
  Obviously maximal value is A[0]+B[0]+C[0]. Candidate of next largest value is oen of {A[1]+B[0]+C[0],A[0]+B[1]+C[0],A[0]+B[0]+C[1]}.
  We want to always query largest from candidates online. Priority queue works for it.
- 
+
  http://drken1215.hatenablog.com/entry/2019/04/07/001000
  https://img.atcoder.jp/abc123/editorial.pdf
  https://twitter.com/torus711/status/1114523526952710144
@@ -83,12 +83,27 @@ template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, con
  https://twitter.com/kyopro_friends/status/1114524642796953600
  https://twitter.com/satanic0258/status/1114533340260917248
  https://twitter.com/nabesan_go/status/1114535121107832833
- 
+
+ 6/3/2020
+
+ 9:00-9:33 solve again
+
  */
 
 const int MAX_N=1e3+1;
 LL A[3][MAX_N];
 int K,N[3];
+
+void solve_again() {
+  REP(i,3)REP(j,N[i]) sort(A[i],A[i]+N[i]),reverse(A[i],A[i]+N[i]);
+  vector<LL> xs;
+  REP(i,N[0])REP(j,N[1]) xs.push_back(A[0][i]+A[1][j]);
+  sort(xs.rbegin(),xs.rend());
+  vector<LL> res;
+  REP(i,min(SZ(xs),K))REP(j,N[2]) res.push_back(xs[i]+A[2][j]);
+  sort(res.rbegin(),res.rend());
+  REP(i,K) cout<<res[i]<<endl;
+}
 
 void solve_semibruteforce() {
   REP(c,3) sort(A[c],A[c]+N[c]),reverse(A[c],A[c]+N[c]);
@@ -126,11 +141,11 @@ int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   cout<<setprecision(12)<<fixed;
-  
+
   REP(i,3) cin>>N[i];
   cin>>K;
   REP(i,3)REP(j,N[i]) cin>>A[i][j];
   solve();
-  
+
   return 0;
 }
