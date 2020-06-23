441c1a2d8a6d49ff0685e9af34a837a7edf1d90e
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-06-22 21:37:40 -0700

    solve CodeFestival2016 final C - Interpretation again

--- solutions/Interpretation.cpp
+++ solutions/Interpretation.cpp
@@ -49,52 +49,69 @@ typedef tuple< int, int, int > III;
 #define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
 #define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
 #define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
-
+// $ cp-batch  Interpretation | diff  Interpretation.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address  Interpretation.cpp && ./a.out
 /*
- 
+
  2018/8/12
- 
+
  23:05-23:17 anslysis
  23:28 got AC after 1RE fix (path compression bug)
- 
+
  Editorials:
   - http://competitive-kivantium.hateblo.jp/entry/2016/11/26/201016
   - https://kimiyuki.net/writeup/algo/atcoder/code-festival-2016-final-c/
- 
+
  Summary:
   - I didn't have idea to reduce to bipartite graph
- 
+
+ 6/22/2020
+
+ 21:17-21:30 solve again
+
  */
 
-// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG x.cpp && ./a.out
+
 const int MAX_N=1e5+1;
 int N,M;
 VI L[MAX_N];
 
-int G;
-int uf[MAX_N];
-int find(int x) { return x==uf[x]?x:uf[x]=find(uf[x]); }
-void unite(int x, int y) {
-  int xx=find(x),yy=find(y);
-  if(xx!=yy) {
-    uf[xx]=yy;
-    --G;
+struct UF {
+public:
+  int N,G/* # of disjoint sets */;
+  UF(int N): N(N) { init(N); }
+  void init(int N) {
+    par=vector<int>(N),sz=vector<int>(N,1);
+    for(int i=0; i<N; ++i) par[i]=i,sz[i]=1;
+    G=N;
   }
-}
-bool solve() {
-  G=N;
-  REP(i,N) uf[i]=i;
-  REP(i,M) REP(j,SZ(L[i])-1) unite(L[i][j],L[i][j+1]);
-//  dump(G);
-//  REP(i,N)dump2(i,find(i));
-  return G==1;
+  int find(int x) { return par[x]==x?x:par[x]=find(par[x]); }
+  int size(int x) { return sz[find(x)]; }
+  bool sameset(int x, int y) { return find(x)==find(y); }
+  int unite(int x, int y) {
+    x=find(x),y=find(y);
+    if(x==y) return x;
+    int p=x,c=y;
+    if(sz[p]<sz[c]) swap(p,c);
+    G--,sz[p]=sz[c]=sz[p]+sz[c];
+    par[c]=p;
+    return p;
+  }
+private:
+  vector<int> par,sz;
+};
+
+void solve() {
+  UF uf(N);
+  REPE(i,M) REP(j,SZ(L[i])-1) uf.unite(L[i][j],L[i][j+1]);
+  cout<<(uf.G==1?"YES":"NO")<<endl;
 }
 
-const string yes="YES",no="NO";
+
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
-  
+
   cin>>N>>M;
   REP(i,N) {
     int K; cin>>K;
@@ -103,6 +120,6 @@ int main() {
       L[l-1].push_back(i);
     }
   }
-  cout<<(solve()?yes:no)<<endl;
+  solve();
   return 0;
 }
