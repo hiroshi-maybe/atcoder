b8124486d6c244a64b638df9fdf8bea1d5ccbb7f
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-07-04 18:34:31 -0700

    solve ABC139 E - League again

--- solutions/League.cpp
+++ solutions/League.cpp
@@ -46,23 +46,60 @@ template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, con
 // $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address League.cpp && ./a.out
 
 /*
- 
+
  9/1/2019
- 
+
  5:26-5:57 AC
- 
+
  https://img.atcoder.jp/abc139/editorial.pdf
  http://kmjp.hatenablog.jp/entry/2019/09/01/1000
- 
+
+ 7/4/2020
+
+ 17:58-18:20 solve again
+
  */
 
 const int MAX_N=1e3+1;
 int A[MAX_N][MAX_N];
 int N;
 
+void solve() {
+  VI ps(N,0);
+  int res=0;
+  while(count(ALL(ps),N-1)<N) {
+    VI X(N,-1);
+    REP(i,N) if(ps[i]<N-1) X[i]=A[i][ps[i]];
+    VI is;
+    REP(i,N) if(X[i]!=-1&&i==X[X[i]]) {
+      is.push_back(i);
+    }
+    assert(SZ(is)%2==0);
+    if(is.empty()) {
+      cout<<-1<<endl;
+      return;
+    }
+    FORR(i,is) ps[i]++;
+    ++res;
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  cin>>N;
+  REP(i,N)REP(j,N-1) cin>>A[i][j],A[i][j]--;
+  solve();
+
+  return 0;
+}
+
 map<II,int> ids;
 VI G[MAX_N*MAX_N];
-void solve() {
+void solve_org() {
   REP(i,N) REP(j,N-2) {
     int a=A[i][j],b=A[i][j+1];
     G[a].push_back(b);
@@ -90,13 +127,13 @@ void solve() {
   cout<<res<<endl;
 }
 
-int main() {
+int main_org() {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   cout<<setprecision(12)<<fixed;
-  
+
   cin>>N;
-  
+
   int id=0;
   REP(i,N)FOR(j,i+1,N) ids[{i,j}]=id++;
   REP(i,N)REP(j,N-1) {
@@ -107,6 +144,6 @@ int main() {
     A[i][j]=ids[{x,y}];
   }
   solve();
-  
+
   return 0;
 }
