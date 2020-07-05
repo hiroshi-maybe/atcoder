bf86af6041a383f82c17a0a8bc5addf9753a3373
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-07-05 11:58:21 -0700

    add solution of ABC173 D - Chat in a Circle

---
+++ solutions/ChatInACircle.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch ChatInACircle | diff ChatInACircle.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address ChatInACircle.cpp && ./a.out
+
+/*
+
+ 7/5/2020
+
+ 5:09-5:23 WA
+ 5:41 give up
+
+ 9:46-10:03 figured out that I was misunderstanding the problem...
+ 10:37 AC
+ 11:54 read editorials
+
+ https://twitter.com/hanseilimak/status/1279823328539078656
+ https://img.atcoder.jp/abc173/editorial.pdf
+ https://www.youtube.com/watch?v=IMwigbYzLbI&t=2034s
+
+ https://twitter.com/maspy_stars/status/1279774609831636992
+
+ */
+
+const int MAX_N=1e6+1;
+LL A[MAX_N];
+int N;
+
+void solve() {
+  sort(A,A+N);
+  LL res=A[N-1];
+  priority_queue<LL> Q;
+  Q.emplace(A[N-2]),Q.emplace(A[N-2]);
+  for(int i=N-3; i>=0; --i) {
+    res+=Q.top(),Q.pop();
+    Q.emplace(A[i]),Q.emplace(A[i]);
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  cin>>N;
+  REP(i,N) cin>>A[i];
+  solve();
+
+  return 0;
+}
