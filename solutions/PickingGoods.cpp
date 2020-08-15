594f9e26301b1260d879699b2f6992de047f20c9
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-08-15 15:18:35 -0700

    solve ABC175 E - Picking Goods

---
+++ solutions/PickingGoods.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+//#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch PickingGoods | diff PickingGoods.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address PickingGoods.cpp && ./a.out
+
+/*
+
+ 8/15/2020
+
+ 6:17-6:45 AC after contest ended
+
+ https://twitter.com/hanseilimak/status/1294753121864790017
+ https://atcoder.jp/contests/abc175/editorial/54
+
+ */
+
+const int MAX_N=3000+1;
+LL V[MAX_N][MAX_N];
+int N,M,K;
+
+//const LL Inf=1e18;
+LL dp[MAX_N][MAX_N][4];
+void solve() {
+  dp[0][0][0]=0;
+  REP(i,N)REP(j,M)REPE(k,3) {
+    if(dp[i][j][k]>0) dump(i,j,k,dp[i][j][k]);
+    // right
+    SMAX(dp[i][j+1][k],dp[i][j][k]);
+    if(k<3) SMAX(dp[i][j+1][k+1],dp[i][j][k]+V[i][j]);
+
+    // down
+    SMAX(dp[i+1][j][0],dp[i][j][k]);
+    if(k<3) SMAX(dp[i+1][j][0],dp[i][j][k]+V[i][j]);
+  }
+  LL res=0;
+  REPE(k,3) {
+    LL a=dp[N-1][M-1][k];
+    dump(k,a);
+    if(k<3) a+=V[N-1][M-1];
+    SMAX(res,a);
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  cin>>N>>M>>K;
+  REP(i,K) {
+    int r,c; LL v; cin>>r>>c>>v;
+    --r,--c;
+    V[r][c]=v;
+  }
+  solve();
+
+  return 0;
+}
