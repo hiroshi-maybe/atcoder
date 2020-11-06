ca3603ae908fba0a122811bb8babc635ac6d88c9
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-11-05 21:02:47 -0800

    solve ABC041 D - 徒競走

---
+++ solutions/FootRace.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch FootRace | diff FootRace.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address FootRace.cpp && ./a.out
+
+/*
+
+ 11/5/2020
+
+ 20:13-20:55 AC
+
+ https://img.atcoder.jp/data/abc/041/editorial.pdf
+
+ */
+
+const int MAX_N=16;
+II A[MAX_N];
+int N,M;
+
+LL dp[1<<MAX_N];
+bool ith(int mask, int i) { return (mask>>i)&1; }
+void solve() {
+  VI ng(1<<N);
+  REP(mask,1<<N) {
+    REP(i,M) {
+      int x,y; tie(x,y)=A[i];
+      if(ith(mask,y)) ng[mask]|=1<<x;
+    }
+  }
+
+  REP(i,N) dp[1<<i]=1;
+  REP(mask,1<<N) {
+    REP(i,N) if(!ith(mask,i)&&!ith(ng[mask],i)) dp[mask|(1<<i)]+=dp[mask];
+  }
+
+  cout<<dp[(1<<N)-1]<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  cin>>N>>M;
+  REP(i,M) {
+    int x,y; cin>>x>>y;
+    --x,--y;
+    A[i]={x,y};
+  }
+  solve();
+
+  return 0;
+}
