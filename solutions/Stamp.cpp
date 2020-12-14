49c52e8854a70e922afe1b6abe1b4dd00f5818d9
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-12-14 10:10:56 +0900

    sovle ABC185 D - Stamp

---
+++ solutions/Stamp.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch Stamp | diff Stamp.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address Stamp.cpp && ./a.out
+
+/*
+
+ 12/13/2020
+
+ 21:28-21:48 AC
+
+ https://atcoder.jp/contests/abc185/editorial/357
+
+ */
+
+const int MAX_N=1e6+1;
+int A[MAX_N];
+int N,M;
+
+void solve() {
+  A[0]=0,A[M+1]=N+1;
+  sort(A,A+M+2);
+  int mind=1e9+100;
+  //dumpC(A,A+M+2);
+  REPE(i,M) if(A[i+1]-A[i]>1) {
+    SMIN(mind,A[i+1]-A[i]-1);
+    //dump(i,A[i+1],A[i],mind);
+  }
+  //dump(mind);
+  int res=0;
+  REPE(i,M) {
+    int d=A[i+1]-A[i];
+    if(d<=1) continue;
+    d-=1;
+    //dump(i,d,(d+mind-1)/mind);
+    res+=(d+mind-1)/mind;
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  cin>>N>>M;
+  REP(i,M) cin>>A[i+1];
+  solve();
+
+  return 0;
+}
