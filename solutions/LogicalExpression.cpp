a8d80ab25ed8edc1744bba1075dca3b404a02e98
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-01-23 18:51:34 -0800

    solve ABC189 D - Logical Expression

---
+++ solutions/LogicalExpression.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch LogicalExpression | diff LogicalExpression.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address LogicalExpression.cpp && ./a.out
+
+/*
+
+ 1/23/2021
+
+ 4:11-4:20 AC
+
+ https://atcoder.jp/contests/abc189/editorial/537
+ https://youtu.be/u2woAs3M1c0?t=3661
+
+ */
+
+const int MAX_N=100;
+int S[MAX_N];
+int N;
+
+LL dp[MAX_N][2];
+void solve() {
+  dp[0][0]=dp[0][1]=1;
+  REP(i,N)REP(j,2) {
+    if(S[i]) {
+      // or
+      dp[i+1][j|0]+=dp[i][j];
+      dp[i+1][j|1]+=dp[i][j];
+    } else {
+      // and
+      dp[i+1][j&0]+=dp[i][j];
+      dp[i+1][j&1]+=dp[i][j];
+    }
+  }
+  cout<<dp[N][1]<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  cin>>N;
+  REP(i,N) {
+    string s; cin>>s;
+    S[i]=s[0]=='O';
+  }
+  solve();
+
+  return 0;
+}
