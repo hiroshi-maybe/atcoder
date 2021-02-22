1f2c0f879a5f8315e19644248a5600c789c182a2
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-02-21 17:54:43 -0800

    solve ARC113 C - String Invasion

---
+++ solutions/StringInvasion.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+//#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch StringInvasion | diff StringInvasion.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address StringInvasion.cpp && ./a.out
+
+/*
+
+ 2/21/2021
+
+ 4:57-5:27 AC
+
+ https://atcoder.jp/contests/arc113/editorial/710
+ https://twitter.com/laycrs/status/1363488854917971968
+
+ */
+
+const int MAX_N=1e6+1;
+string S;
+int N;
+
+int cum[MAX_N][26];
+//int nxt[MAX_N][26];
+void solve() {
+  N=SZ(S);
+  REP(i,N) {
+    REP(c,26) cum[i+1][c]=cum[i][c];
+    cum[i+1][S[i]-'a']+=1;
+  }
+
+  /*
+  REP(i,26) nxt[N][i]=N;
+  for(int i=N; i>=1; --i) {
+    REP(c,26) nxt[i-1][c]=nxt[i][c];
+    nxt[i-1][S[i]-'a']=i;
+  }*/
+
+  LL res=0;
+  int pre=-1,prep=N;
+  for(int i=N-1; i>=0; --i) {
+    if(i<N-1&&S[i]==S[i+1]) {
+      pre=S[i]-'a',prep=i;
+      continue;
+    }
+
+    if(i<N-1&&i>0&&S[i-1]==S[i]&&S[i+1]!=S[i]) {
+      int cur=S[i]-'a';
+      dump(i,cur,pre,prep,res);
+      if(cur!=pre) res+=N-i-1;
+      else res+=prep-i-1;
+      dump(i,cur,pre,prep,res);
+      res-=cum[prep][cur]-cum[i+1][cur];
+      dump(i,cur,pre,prep,res,cum[prep][cur],cum[i+1][cur]);
+    }
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  cin>>S;
+  solve();
+
+  return 0;
+}
