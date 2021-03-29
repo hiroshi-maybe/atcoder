b0d7ec1d28e4edeb826b7b9bbb0c2f8bb983d79b
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-03-28 23:22:35 -0700

    solve Code Festival 2014 QRB D - 登山家

---
+++ solutions/Climber.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch Climber | diff Climber.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address Climber.cpp && ./a.out
+
+/*
+
+ 3/27/2021
+
+ 22:10-22:22 AC
+
+ https://www.slideshare.net/chokudai/codefestival2014qual-b
+
+ */
+
+const int MAX_N=1e6+1;
+int H[MAX_N];
+int N;
+
+VI f(VI A) {
+  const int Inf=1e8;
+  vector<II> st; st.emplace_back(Inf,N);
+  VI res(SZ(A));
+  for(int i=SZ(A)-1; i>=0; --i) {
+    while(st.back().first<=A[i]) st.pop_back();
+    res[i]=st.back().second-i-1;
+    st.emplace_back(A[i],i);
+  }
+  return res;
+}
+
+void solve() {
+  VI xs=f(VI(H,H+N));
+  VI A(H,H+N); reverse(ALL(A));
+  VI rev=f(A);
+
+  VI res(N);
+  REP(i,N) res[i]=xs[i]+rev[N-1-i];
+  REP(i,N) cout<<res[i]<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  cin>>N;
+  REP(i,N) cin>>H[i];
+  solve();
+
+  return 0;
+}
