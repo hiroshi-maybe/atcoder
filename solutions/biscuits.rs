c846d1d66dd9251593a9e8f5257805bc40c3aa1e
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-10-29 21:38:32 -0700

    solve AGC017 A - Biscuits

---
+++ solutions/biscuits.rs
@@ -0,0 +1,126 @@
+#![allow(unused_macros, unused_imports)]
+use std::cmp::*;
+use std::collections::*;
+
+// $ cp-batch biscuits | diff biscuits.out -
+// $ cargo run --bin biscuits
+
+///
+/// 10/28/2021
+///
+/// 22:56-23:02 AC
+///
+/// 10/29/2021
+///
+/// Add math solution
+///
+/// https://img.atcoder.jp/agc017/editorial.pdf
+///
+
+#[allow(dead_code)]
+fn solve_dp(n: usize, p: usize, a: &[usize]) -> u64 {
+    let mut dp = vvec![0u64; n+1; 2];
+    dp[0][0] = 1;
+    for i in 0..n {
+        for p in 0..2 {
+            let cur = dp[i][p];
+            dp[i + 1][(p + a[i]) % 2] += cur;
+            dp[i + 1][p] += cur;
+        }
+    }
+    dp[n][p]
+}
+
+fn solve_math(n: usize, p: usize, a: &[usize]) -> u64 {
+    if a.iter().filter(|a| *a % 2 == 0).count() == n {
+        return if p == 0 { 1 << n } else { 0 };
+    }
+
+    1 << (n - 1)
+}
+
+fn main() {
+    setup_out!(put, puts);
+
+    let (n, p) = readln!(usize, usize);
+    let a = readln!([usize]);
+
+    let res = solve_math(n, p, &a);
+
+    puts!("{}", res);
+}
+
+use crate::cplib::io::*;
+use crate::cplib::minmax::*;
+use crate::cplib::vec::*;
+// region: cplib
+#[rustfmt::skip]
+#[allow(dead_code)]
+pub mod cplib {
+pub mod io {
+	macro_rules! _with_dollar_sign { ($($body:tt)*) => { macro_rules! __with_dollar_sign { $($body)* } __with_dollar_sign!($); }}
+	macro_rules! setup_out {
+		($fn:ident,$fn_s:ident) => {
+			use std::io::Write;
+			let out = std::io::stdout();
+			let mut out = ::std::io::BufWriter::new(out.lock());
+				_with_dollar_sign! { ($d:tt) => {
+				macro_rules! $fn { ($d($format:tt)*) => { let _ = write!(out,$d($format)*); } }
+				macro_rules! $fn_s { ($d($format:tt)*) => { let _ = writeln!(out,$d($format)*); } }
+			}}
+		};
+	}
+	macro_rules! _epr { ($v:expr $(,)?) => {eprint!("{} = {:?}, ", stringify!($v), $v)}; }
+	macro_rules! dbgln { ($($val:expr),*) => {{ eprint!("[{}:{}] ", file!(), line!()); ($(_epr!($val)),*); eprintln!(); }}; }
+	pub fn readln_str() -> String {
+		let mut line = String::new();
+		::std::io::stdin().read_line(&mut line).unwrap_or_else(|e| panic!("{}", e));
+		line
+	}
+	macro_rules! _read {
+		($it:ident; [char]) => { _read!($it; String).chars().collect::<Vec<_>>() };
+		($it:ident; [u8]) => { Vec::from(_read!($it; String).into_bytes()) };
+		($it:ident; usize1) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1 };
+		($it:ident; [usize1]) => { $it.map(|s| s.parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1).collect::<Vec<_>>() };
+		($it:ident; [$t:ty]) => { $it.map(|s| s.parse::<$t>().unwrap_or_else(|e| panic!("{}", e))).collect::<Vec<_>>() };
+		($it:ident; $t:ty) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<$t>().unwrap_or_else(|e| panic!("{}", e)) };
+		($it:ident; $($t:tt),+) => { ($(_read!($it; $t)),*) };
+	}
+	macro_rules! readlns {
+		($($t:tt),*; $n:expr) => {{ let stdin = ::std::io::stdin();
+			::std::io::BufRead::lines(stdin.lock()).take($n).map(|line| {
+				let line = line.unwrap(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*)
+			}).collect::<Vec<_>>()
+		}};
+	}
+	macro_rules! readln {
+		($($t:tt),*) => {{ let line = cplib::io::readln_str(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*) }};
+	}
+	pub(crate) use {readlns, readln, setup_out, dbgln, _with_dollar_sign, _epr, _read};
+}
+pub mod minmax {
+	pub trait SetMinMax {
+		fn setmin<'a>(&'a mut self, other: Self) -> (bool, &'a Self);
+		fn setmax<'a>(&'a mut self, other: Self) -> (bool, &'a Self);
+	}
+	macro_rules! _set { ($self:ident, $cmp:tt, $other:ident) => {{
+			let update = $other $cmp *$self;
+			if update { *$self = $other; }
+			(update, $self)
+	}}; }
+	impl<T> SetMinMax for T where T: PartialOrd {
+		fn setmin<'a>(&'a mut self, other: T) -> (bool, &'a Self) { _set!(self, <, other) }
+		fn setmax<'a>(&'a mut self, other: T) -> (bool, &'a Self) { _set!(self, >, other) }
+	}
+}
+pub mod vec {
+	pub trait CollectVec: Iterator { fn collect_vec(self) -> Vec<Self::Item> where Self: Sized { self.collect() } }
+	impl<T> CollectVec for T where T: Iterator {}
+	macro_rules! vvec {
+		($v:expr; $n:expr) => { Vec::from(vec![$v; $n]) };
+		($v:expr; $n:expr $(; $ns:expr)+) => { Vec::from(vec![vvec![$v $(; $ns)*]; $n]) };
+	}
+	pub(crate) use vvec;
+}
+}
+// endregion: cplib
