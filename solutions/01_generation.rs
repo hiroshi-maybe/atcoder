40a6f3aa2a50d91b565788d0747c2e5b59b703cc
Hiroshi Kori <hiroshi.maybe@gmail.com>
2022-04-17 14:10:37 -0700

    add solution of ARC138 B - 01 Generation

---
+++ solutions/01_generation.rs
@@ -0,0 +1,140 @@
+#![allow(unused_macros, unused_imports)]
+use std::cmp::*;
+use std::collections::*;
+
+// $ cp-batch 01_generation | diff 01_generation.out -
+// $ cargo run --bin 01_generation
+
+///
+/// 4/9/2022
+///
+/// 11:20-12:00 give up
+///
+/// 4/16/2022
+///
+/// 20:05-20:25 pause
+///
+/// 4/17/2022
+///
+/// 13:10-14:06 read editorial and AC
+///
+/// https://docs.google.com/document/d/1Yq384PEooRVsmlzB103YMLWwE_JhPN8AbxZagNrF_vY/edit#bookmark=id.35iv6vsh23gx
+/// https://docs.google.com/document/d/1Yq384PEooRVsmlzB103YMLWwE_JhPN8AbxZagNrF_vY/edit#bookmark=id.peltuflhcxch
+///
+/// https://atcoder.jp/contests/arc138/editorial/3743
+///
+/// https://twitter.com/tatyam_prime/status/1512794333577375747
+/// https://twitter.com/ChineristA/status/1512793017006313479
+/// https://twitter.com/tomerun/status/1512794018979397636
+/// https://twitter.com/satanic0258/status/1512795911466143745
+/// https://twitter.com/soda628496/status/1512795342546546696
+/// https://twitter.com/dnaTaK/status/1512795492291575812
+/// https://twitter.com/cunitac/status/1512804885368291330
+/// https://twitter.com/vi_24E/status/1512793788884086787
+/// https://twitter.com/hamamu_kyopro/status/1512799992091611137
+///
+
+fn solve() -> bool {
+    let _n = readln!(usize);
+    let a = readln!([usize]);
+
+    let mut flipped = false;
+    let mut q = a.iter().copied().collect::<VecDeque<_>>();
+    while let Some(&tail) = q.back() {
+        let val = |v: usize| v ^ (flipped as usize);
+        if val(tail) == 0 {
+            q.pop_back();
+        } else {
+            let fr = val(q.pop_front().unwrap());
+            if fr != 0 {
+                return false;
+            } else {
+                flipped = !flipped;
+            }
+        }
+    }
+
+    true
+}
+
+fn main() {
+    setup_out!(put, puts);
+
+    let res = solve();
+    puts!("{}", if res { "Yes" } else { "No" });
+}
+
+use crate::cplib::io::*;
+use crate::cplib::minmax::*;
+use crate::cplib::vec::*;
+// region: cplib
+#[rustfmt::skip]
+#[allow(dead_code)]
+pub mod cplib {
+pub mod io {
+	macro_rules! _with_dollar_sign { ($($body:tt)*) => { macro_rules! __with_dollar_sign { $($body)* } __with_dollar_sign!($); }}
+	macro_rules! setup_out {
+		($fn:ident,$fn_s:ident) => {
+			use std::io::Write;
+			let out = std::io::stdout();
+			let mut out = ::std::io::BufWriter::new(out.lock());
+				_with_dollar_sign! { ($d:tt) => {
+				macro_rules! $fn { ($d($format:tt)*) => { let _ = write!(out,$d($format)*); } }
+				macro_rules! $fn_s { ($d($format:tt)*) => { let _ = writeln!(out,$d($format)*); } }
+			}}
+		};
+	}
+	macro_rules! _epr { ($v:expr $(,)?) => {eprint!("{} = {:?}, ", stringify!($v), $v)}; }
+	macro_rules! dbgln { ($($val:expr),*) => {{ eprint!("[{}:{}] ", file!(), line!()); ($(_epr!($val)),*); eprintln!(); }}; }
+	pub fn readln_str() -> String {
+		let mut line = String::new();
+		::std::io::stdin().read_line(&mut line).unwrap_or_else(|e| panic!("{}", e));
+		line
+	}
+	macro_rules! _read {
+		($it:ident; [char]) => { _read!($it; String).chars().collect::<Vec<_>>() };
+		($it:ident; [u8]) => { Vec::from(_read!($it; String).into_bytes()) };
+		($it:ident; usize1) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1 };
+		($it:ident; [usize1]) => { $it.map(|s| s.parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1).collect::<Vec<_>>() };
+		($it:ident; [$t:ty]) => { $it.map(|s| s.parse::<$t>().unwrap_or_else(|e| panic!("{}", e))).collect::<Vec<_>>() };
+		($it:ident; $t:ty) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<$t>().unwrap_or_else(|e| panic!("{}", e)) };
+		($it:ident; $($t:tt),+) => { ($(_read!($it; $t)),*) };
+	}
+	macro_rules! readlns {
+		($($t:tt),*; $n:expr) => {{ let stdin = ::std::io::stdin();
+			::std::io::BufRead::lines(stdin.lock()).take($n).map(|line| {
+				let line = line.unwrap(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*)
+			}).collect::<Vec<_>>()
+		}};
+	}
+	macro_rules! readln {
+		($($t:tt),*) => {{ let line = cplib::io::readln_str(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*) }};
+	}
+	pub(crate) use {readlns, readln, setup_out, dbgln, _with_dollar_sign, _epr, _read};
+}
+pub mod minmax {
+	pub trait SetMinMax {
+		fn setmin(&mut self, other: Self) -> bool;
+		fn setmax(&mut self, other: Self) -> bool;
+	}
+	macro_rules! _set { ($self:ident, $cmp:tt, $other:ident) => {{
+			let update = $other $cmp *$self;
+			if update { *$self = $other; }
+			update
+	}}; }
+	impl<T> SetMinMax for T where T: PartialOrd {
+		fn setmin(&mut self, other: T) -> bool { _set!(self, <, other) }
+		fn setmax(&mut self, other: T) -> bool { _set!(self, >, other) }
+	}
+}
+pub mod vec {
+	pub trait CollectVec: Iterator { fn collect_vec(self) -> Vec<Self::Item> where Self: Sized { self.collect() } }
+	impl<T> CollectVec for T where T: Iterator {}
+	macro_rules! vvec {
+		($v:expr; $n:expr) => { Vec::from(vec![$v; $n]) };
+		($v:expr; $n:expr $(; $ns:expr)+) => { Vec::from(vec![vvec![$v $(; $ns)*]; $n]) };
+	}
+	pub(crate) use vvec;
+}
+}
+// endregion: cplib
