979d09853138eb55ba3de824e1af5524f273c02d
Hiroshi Kori <hiroshi.maybe@gmail.com>
2022-06-19 17:42:59 +0900

    solve ABC256 E - Takahashi's Anguish

---
+++ solutions/takahashis_anguish.rs
@@ -0,0 +1,150 @@
+#![allow(unused_macros, unused_imports)]
+use std::cmp::*;
+use std::collections::*;
+
+// $ cp-batch takahashis_anguish | diff takahashis_anguish.out -
+// $ cargo run --bin takahashis_anguish
+
+///
+/// 6/19/2022
+///
+/// 16:42-17:01 AC
+///
+/// https://atcoder.jp/contests/abc256/editorial/4135
+///
+
+fn dfs(u: usize, viz: &mut Vec<bool>, g: &Vec<Vec<(usize, i64)>>, res: &mut i64) {
+    if viz[u] {
+        return;
+    }
+    viz[u] = true;
+
+    for &(v, cost) in &g[u] {
+        res.setmin(cost);
+        dfs(v, viz, g, res);
+    }
+}
+
+fn solve() -> i64 {
+    let n = readln!(usize);
+    let xs = readln!([usize1]);
+    let cs = readln!([i64]);
+
+    let mut g = vec![vec![]; n];
+    let mut in_deg = vec![0; n];
+    for u in 0..n {
+        g[u].push((xs[u], cs[u]));
+        in_deg[xs[u]] += 1;
+    }
+
+    let mut q = VecDeque::new();
+    for u in 0..n {
+        if in_deg[u] == 0 {
+            q.push_back(u);
+        }
+    }
+    // dbgln!(in_deg);
+    while let Some(u) = q.pop_front() {
+        for &(v, _) in &g[u] {
+            in_deg[v] -= 1;
+            if in_deg[v] == 0 {
+                q.push_back(v);
+            }
+        }
+    }
+
+    // dbgln!(in_deg);
+    let mut viz = vec![false; n];
+    let mut ans = 0;
+    for u in (0..n).filter(|&u| in_deg[u] > 0) {
+        if !viz[u] {
+            let mut res = i64::max_value();
+            dfs(u, &mut viz, &g, &mut res);
+            ans += res;
+        }
+    }
+
+    ans
+}
+
+fn main() {
+    setup_out!(put, puts);
+
+    let res = solve();
+    puts!("{}", res);
+}
+
+use crate::cplib::io::*;
+use crate::cplib::minmax::*;
+use crate::cplib::vec::*;
+// region: cplib
+#[rustfmt::skip]
+#[allow(dead_code)]
+pub mod cplib {
+pub mod io {
+	macro_rules! _with_dollar_sign { ($($body:tt)*) => { macro_rules! __with_dollar_sign { $($body)* } __with_dollar_sign!($); }}
+	macro_rules! setup_out {
+		($fn:ident,$fn_s:ident) => {
+			use std::io::Write;
+			let out = std::io::stdout();
+			let mut out = ::std::io::BufWriter::new(out.lock());
+				_with_dollar_sign! { ($d:tt) => {
+				macro_rules! $fn { ($d($format:tt)*) => { let _ = write!(out,$d($format)*); } }
+				macro_rules! $fn_s { ($d($format:tt)*) => { let _ = writeln!(out,$d($format)*); } }
+			}}
+		};
+	}
+	macro_rules! _epr { ($v:expr $(,)?) => {eprint!("{} = {:?}, ", stringify!($v), $v)}; }
+	macro_rules! dbgln { ($($val:expr),*) => {{ eprint!("[{}:{}] ", file!(), line!()); ($(_epr!($val)),*); eprintln!(); }}; }
+	pub fn readln_str() -> String {
+		let mut line = String::new();
+		::std::io::stdin().read_line(&mut line).unwrap_or_else(|e| panic!("{}", e));
+		line
+	}
+	macro_rules! _read {
+		($it:ident; [char]) => { _read!($it; String).chars().collect::<Vec<_>>() };
+		($it:ident; [u8]) => { Vec::from(_read!($it; String).into_bytes()) };
+		($it:ident; usize1) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1 };
+		($it:ident; [usize1]) => { $it.map(|s| s.parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1).collect::<Vec<_>>() };
+		($it:ident; [$t:ty]) => { $it.map(|s| s.parse::<$t>().unwrap_or_else(|e| panic!("{}", e))).collect::<Vec<_>>() };
+		($it:ident; $t:ty) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<$t>().unwrap_or_else(|e| panic!("{}", e)) };
+		($it:ident; $($t:tt),+) => { ($(_read!($it; $t)),*) };
+	}
+	macro_rules! readlns {
+		($($t:tt),*; $n:expr) => {{ let stdin = ::std::io::stdin();
+			::std::io::BufRead::lines(stdin.lock()).take($n).map(|line| {
+				let line = line.unwrap(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*)
+			}).collect::<Vec<_>>()
+		}};
+	}
+	macro_rules! readln {
+		($($t:tt),*) => {{ let line = cplib::io::readln_str(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*) }};
+	}
+	pub(crate) use {readlns, readln, setup_out, dbgln, _with_dollar_sign, _epr, _read};
+}
+pub mod minmax {
+	pub trait SetMinMax {
+		fn setmin(&mut self, other: Self) -> bool;
+		fn setmax(&mut self, other: Self) -> bool;
+	}
+	macro_rules! _set { ($self:ident, $cmp:tt, $other:ident) => {{
+			let update = $other $cmp *$self;
+			if update { *$self = $other; }
+			update
+	}}; }
+	impl<T> SetMinMax for T where T: PartialOrd {
+		fn setmin(&mut self, other: T) -> bool { _set!(self, <, other) }
+		fn setmax(&mut self, other: T) -> bool { _set!(self, >, other) }
+	}
+}
+pub mod vec {
+	pub trait CollectVec: Iterator { fn collect_vec(self) -> Vec<Self::Item> where Self: Sized { self.collect() } }
+	impl<T> CollectVec for T where T: Iterator {}
+	macro_rules! vvec {
+		($v:expr; $n:expr) => { Vec::from(vec![$v; $n]) };
+		($v:expr; $n:expr $(; $ns:expr)+) => { Vec::from(vec![vvec![$v $(; $ns)*]; $n]) };
+	}
+	pub(crate) use vvec;
+}
+}
+// endregion: cplib
